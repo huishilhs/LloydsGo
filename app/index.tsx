4978bcb958ff8e123b701176eabb68b9daa0a5e8
huishilhs <87135201+huishilhs@users.noreply.github.com>
2025-02-17 18:52:07 +0000

    supabase authentication
    - connect to supabase
    - add sign up
    - add sign in

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,26 @@
-import { Link } from 'expo-router';
-import { View, Text } from 'react-native';
+import { useState, useEffect } from 'react'
+import { supabase } from '../lib/supabase'
+import Auth from '../components/Auth'
+import Account from '../components/Account'
+import { View } from 'react-native'
+import { Session } from '@supabase/supabase-js'
 
-export default function LandingPage() {
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
     <View>
-      <Text>Welcome to the App!</Text>
-      <Link href="/(tabs)/home">Go to Tabs</Link>
+      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
     </View>
-  );
+  )
 }
