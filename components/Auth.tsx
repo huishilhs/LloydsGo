aef9f99a8dab2cc6529c71bf35d517d62a87bc15
huishilhs <87135201+huishilhs@users.noreply.github.com>
2025-04-24 21:55:57 +0100

    refactor: fetch user's data
    - added supabase fetch to get user's name
    - deleted sign up function

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -34,20 +34,6 @@ export default function Auth() {
         setLoading(false)
     }
 
-    async function signUpWithEmail() {
-        setLoading(true)
-        const {
-            data: { session },
-            error,
-        } = await supabase.auth.signUp({
-            email: email,
-            password: password,
-        })
-
-        if (error) Alert.alert(error.message)
-        if (!session) Alert.alert('Please check your inbox for email verification!')
-        setLoading(false)
-    }
 
     return (
         <View style={styles.container}>
